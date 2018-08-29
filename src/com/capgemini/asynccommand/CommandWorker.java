60f1bc89aa9c333fff3405d42ceb7eaf7c3b3b9f
GuillaumeLD <g.ledanvic@gmail.com>
2018-08-29 13:52:14 +0200

    list function

--- src/com/capgemini/asynccommand/CommandWorker.java
+++ src/com/capgemini/asynccommand/CommandWorker.java
@@ -1,7 +1,9 @@
 package com.capgemini.asynccommand;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class CommandWorker {
-	List functionList = new List<Command<Object,Object>>();
+	List<Function<Object, Object>> functionList = new ArrayList<Function<Object,Object>>();
 }
