7c055772005aba14b54ee773b6e167dddf7ba3a3
GuillaumeLD <g.ledanvic@gmail.com>
2018-08-29 16:03:56 +0200

    Add function

--- src/com/capgemini/asynccommand/CommandWorker.java
+++ src/com/capgemini/asynccommand/CommandWorker.java
@@ -4,6 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
-public class CommandWorker {
+public class CommandWorker implements Command {
+	
 	List<Function<Object, Object>> functionList = new ArrayList<Function<Object,Object>>();
+	
+	@Override
+	public void execute() {
+		
+	}
+	
+	public int currentIndex() {
+		
+		return 0;
+	}
+	
+	public void addFunction(Function<Object,Object> func) {
+		functionList.add(func);
+	}
+
 }
